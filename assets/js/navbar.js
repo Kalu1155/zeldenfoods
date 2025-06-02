0d9f706ffc1bac525d550a51747440619a454f12
Kalu David <148062083+Kalu1155@users.noreply.github.com>
2025-06-02 10:54:35 +0100

    first commit

---
+++ assets/js/navbar.js
@@ -0,0 +1,101 @@
+const navbar = document.createElement("nav");
+navbar.innerHTML = `
+            <div class="header-top">
+
+                <div class="auto-container">
+                    <div class="top-info">
+                        <ul class="info-list clearfix">
+                            <li>
+                                <i class="flaticon-location-pin"></i>
+                                219 Bedford Street Birmingham, AL 35211
+                            </li>
+                            <li>
+                                <i class="flaticon-envelope"></i>
+                                <a href="mailto:info@example.com">info@example.com</a>
+                            </li>
+                            <li class="phone">
+                                <i class="flaticon-dial"></i>
+                                <a href="tel:234554657345">+234 554 657 345</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="header-upper">
+                <div class="auto-container">
+                    <div class="outer-box clearfix">
+                        <div class="logo-box">
+                            <figure class="logo"><a href="index.html"><img src="assets/images/logo.png" alt=""></a></figure>
+                        </div>
+                        <div class="menu-area pull-right">
+                            <!--Mobile Navigation Toggler-->
+                            <div class="mobile-nav-toggler">
+                                <i class="icon-bar"></i>
+                                <i class="icon-bar"></i>
+                                <i class="icon-bar"></i>
+                            </div>
+                            <nav class="main-menu navbar-expand-md navbar-light">
+                                <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                                    <ul class="navigation clearfix">
+                                        <li class="current dropdown"><a href="index.html">Home</a>
+                                        </li> 
+                                        <li><a href="about.html">About</a></li> 
+                                        <li class="dropdown"><a href="index.html">Products</a>
+                                        </li>
+                                        <li class="dropdown"><a href="contact.html">Contact</a>
+                                           
+                                        </li>    
+                                        <li class="dropdown"><a href="index.html">Team</a>
+                                        </li> 
+                                        <li><a href="faq.html">FAQ</a></li> 
+                                        <li><a href="faq.html">Testimonies</a></li>              
+                                    </ul>
+                                </div>
+                            </nav>
+                            <ul class="menu-right-content pull-left clearfix">
+                                <li class="user-box"><a href="index.html"><i class="flaticon-user-symbol-of-thin-outline"></i></a></li>
+                                <li class="search-box-outer">
+                                    <div class="dropdown">
+                                        <button class="search-box-btn" type="button" id="dropdownMenu3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="flaticon-search"></span></button>
+                                        <ul class="dropdown-menu pull-right search-panel" aria-labelledby="dropdownMenu3">
+                                            <li class="panel-outer">
+                                                <div class="form-container">
+                                                    <form method="post" action="https://azim.commonsupport.com/Carneshop/blog.html">
+                                                        <div class="form-group">
+                                                            <input type="search" name="field-name" value="" placeholder="Search...." required="">
+                                                            <button type="submit" class="search-btn"><span class="fas fa-search"></span></button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li class="cart-box"><a href="shop-1.html"><i class="flaticon-shopping-cart-1"></i><span>0</span></a></li>
+                                <li class="nav-box">
+                                    <div class="nav-btn nav-toggler navSidebar-button clearfix">
+                                        <i class="flaticon-list"></i>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="sticky-header">
+                <div class="auto-container">
+                    <div class="outer-box clearfix">
+                        <figure class="logo-box pull-left"><a href="index.html"><img src="assets/images/small-logo.png" alt=""></a></figure>
+                        <div class="menu-area pull-right">
+                            <nav class="main-menu clearfix">
+                                <!--Keep This Empty / Menu will come through Javascript-->
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                
+                </div>
+`;
+
+document.getElementById("navbar-root").appendChild(navbar);
